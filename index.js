303db4f3a2a2f7a2140422a652e6c0cf6ef24851
arslanramzan <arslan@droobihealth.com>
2022-06-13 12:41:19 +0500

    Initilize setup and setup some routes

---
+++ index.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");  //require dotenv package
+dotenv.config({ path: "./config.env" }); //import config.env file
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
---
+++ routes/routes.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const Model = require('../models/model');
+const router = express.Router();
+import S3 from 'react-aws-s3';
+
+
+// const config = {
+//     bucketName: process.env.REACT_APP_BUCKET_NAME,
+//     region: process.env.REACT_APP_REGION,
+//     accessKeyId: process.env.REACT_APP_ACCESS,
+//     secretAccessKey: process.env.REACT_APP_SECRET,
+// }
+
+
+//Post Method
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age,
+        experiences: req.body.experiences
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Update by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
+router.post('/post-image', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+    })
+
+    try {
+        const ReactS3Client = new S3(config);
+        // the name of the file uploaded is used to upload it to S3
+        ReactS3Client
+        .uploadFile(file, file.name)
+        .then(data => console.log(data.location))
+        .catch(err => console.error(err))
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+module.exports = router;
