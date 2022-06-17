5dd29089c3fc5c98d1bfd586c26be0304939cd0c
arslanramzan <arslan@droobihealth.com>
2022-06-17 12:23:56 +0500

    remove ommented code

--- routes/routes.js
+++ routes/routes.js
@@ -65,55 +65,6 @@ router.get('/getOne/:id', async (req, res) => {
     }
 })
 
-// router.post('/images', validateImageType, validateImageExtension, validateImageObject, validate, async (req, res, next) =>{
-//     const base64Image = req.body.image;
-//     const imageName = req.body.name;
-//     const type = req.body.type;
-//     let response;
-
-//     try {
-//         response = await upload(imageName, base64Image);
-//     } catch (err) {
-//         console.error(`Error uploading image: ${err.message}`);
-//         return next(new Error(`Error uploading image: ${imageName}`));
-//     }
-
-//     res.send({link: response});
-// })
-
-// router.post('/images', async (req, res, next) =>{
-//     // const base64Image = req.body.image;
-//     // const imageName = req.body.name;
-//     // const type = req.body.type;
-//     let response;
-
-//     // try {
-//     //     response = await upload(imageName, base64Image);
-//     // } catch (err) {
-//     //     console.error(`Error uploading image: ${err.message}`);
-//     //     return next(new Error(`Error uploading image: ${imageName}`));
-//     // }
-
-//     // res.send({link: response});
-//     console.log(`File entered successfully`, path.resolve(req.body.name));
-//     const fileContent = fs.readFileSync(path.resolve(req.body.name));
-//     console.log('fileContent ', fileContent)
-//     // Setting up S3 upload parameters
-//     const params = {
-//         Bucket: "glints-assets",
-//         Key: req.body.name, // File name you want to save as in S3
-//         Body: fileContent
-//     };
-
-//     // Uploading files to the bucket
-//     s3.upload(params, function(err, data) {
-//         if (err) {
-//             throw err;
-//         }
-//         console.log(`File uploaded successfully. ${data.Location}`);
-//     });
-// })
-
 router.post('/post-images', async (req, res, next) =>{
     const base64Image = req.body.image;
     const imageName = req.body.name;
@@ -132,42 +83,4 @@ router.post('/post-images', async (req, res, next) =>{
     res.send({link: response});
 })
 
-// router.post('/post-image', async (req, res) => {
-//     const data = new Model({
-//         name: req.body.name,
-//     })
-
-//     try {
-//         const ReactS3Client = new S3(config);
-//         // the name of the file uploaded is used to upload it to S3
-//         ReactS3Client
-//         .uploadFile(file, file.name)
-//         .then(data => console.log(data.location))
-//         .catch(err => console.error(err))
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// })
-
 module.exports = router;
-
-// router.post('/post-image', async (req, res) => {
-//     const data = new Model({
-//         name: req.body.name,
-//     })
-
-//     try {
-//         const ReactS3Client = new S3(config);
-//         // the name of the file uploaded is used to upload it to S3
-//         ReactS3Client
-//         .uploadFile(file, file.name)
-//         .then(data => console.log(data.location))
-//         .catch(err => console.error(err))
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// })
-
-// module.exports = router;
--- services/imagesService.js
+++ services/imagesService.js
@@ -1,14 +1,6 @@
 const AWS = require('aws-sdk');
-// const BUCKET_NAME = process.env.IMAGES_BUCKET;
 const s3 = new AWS.S3({});
 
-/**
- * @description Uploads an image to S3
- * @param imageName Image name
- * @param base64Image Image body converted to base 64
- * @param type Image type
- * @return string S3 image URL or error accordingly
- */
 async function upload(imageName, base64Image, type) {
 console.log('process.env.AWS_ACCESS_KEY_ID ', process.env.AWS_ACCESS_KEY_ID)
 AWS.config.update({
@@ -37,11 +29,6 @@ AWS.config.update({
 
     return data.Location;
 }
-/**
- * @description Promise an upload to S3
- * @param params S3 bucket params
- * @return data/err S3 response object
- */
 function promiseUpload(params) {
     return new Promise(function (resolve, reject) {
         s3.upload(params, function (err, data) {
